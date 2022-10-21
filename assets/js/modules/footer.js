5b6669bc863110783a6fd286af6dcf9e3e376a23
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-10-21 13:20:50 -0300

    styled footer

--- assets/js/modules/footer.js
+++ assets/js/modules/footer.js
@@ -2,41 +2,42 @@ export function footer(){
   const header = document.querySelector("footer");
 
   const footer = `
-  <section>
+  <section class="l-contact">
     <section>
-      <h2>Rede Sociais</h2>
+      <h2 class="c-title">Rede Sociais</h2>
       <span>Instagram</span>
       <span>Facebook</span>
       <span>Linkedin</span>
     </section>
 
     <form>
-      <h2>Contatos</h2>
-      <div>
-        <label>Nome</label>
-        <input/>
+      <h2 class="c-title">Contatos</h2>
+
+      <div class="c-input__container">
+        <input class="c-input" placeholder=" "/>
+        <label class="c-input__label">Nome</label>
       </div>
 
-      <div>
-        <label>Sobrenome</label>
-        <input/>
+      <div class="c-input__container">
+        <input class="c-input" placeholder=" "/>
+        <label class="c-input__label">Sobrenome</label>
       </div>
 
-      <div>
-        <label>E-mail</label>
-        <input/>
+      <div class="c-input__container">
+        <input class="c-input" type="email" placeholder=" " />
+        <label class="c-input__label">E-mail</label>
       </div>
 
-      <div>
-        <label>Mensagem</label>
-        <textarea rows="4"></textarea>
+      <div class="c-input__container">
+        <textarea class="c-textarea" type="message" placeholder=" "></textarea>
+        <label class="c-input__label">Mensagem</label>
       </div>
 
       <button class="c-button">Enviar</button>
     </form>
   </section>
 
-  <section>Copyright</section>
+  <section class="c-copyright">&copy; Copyright</section>
   
   `;
 
