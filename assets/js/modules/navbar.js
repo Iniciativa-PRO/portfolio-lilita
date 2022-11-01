60f19e432ad52ba76d150f6b39999ce060dc7c99
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-10-31 21:38:51 -0300

    fixed bugs

--- assets/js/modules/navbar.js
+++ assets/js/modules/navbar.js
@@ -2,18 +2,20 @@ export function navbar(){
   const header = document.querySelector("header");
 
   const navbar = `
-  <img class="c-logo" src="/assets/img/logoValerie.png" alt="Logomarca Valerie Designs" height="40px">
+  <section class="l-container">
+    <nav class="c-navbar">
+      <img class="c-logo" src="/assets/img/logoValerie.png" alt="Logomarca Valerie Designs" height="40px">
 
-  <nav class="c-nav-bar">
-    <ul class="nav__links">
-      <li><a href="/">Accueil</a></li>
-      <li><a href="/page/collaborations/index.html">Collaborations</a></li>
-      <li><a href="/page/formatricedespagnol/index.html">Formatrice d'espagnol</a></li>
-      <li><a href="/page/artsplastiques/index.html">Arts Plastiques</a></li>
-      <li><a href="/page/designgraphique/index.html">Design Graphique</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </nav>
+      <ul class="nav__links">
+        <li class="nav__list"><a class="nav__item" href="/">Accueil</a></li>
+        <li class="nav__list"><a class="nav__item" href="/page/collaborations/index.html">Collaborations</a></li>
+        <li class="nav__list"><a class="nav__item" href="/page/formatricedespagnol/index.html">Formatrice d'espagnol</a></li>
+        <li class="nav__list"><a class="nav__item" href="/page/artsplastiques/index.html">Arts Plastiques</a></li>
+        <li class="nav__list"><a class="nav__item" href="/page/designgraphique/index.html">Design Graphique</a></li>
+        <li class="nav__list"><a class="nav__item" href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </section>
   `;
 
   header.insertAdjacentHTML("afterbegin", navbar);
